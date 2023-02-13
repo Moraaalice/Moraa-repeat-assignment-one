b80e07293f8c81dc8a1e5146583615be6a4ce869
Alice <alicemoraaongongo@gmail.com>
2023-02-13 15:55:56 +0300

    repetition of assinment

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+   var fullName = "Alice Moraa"
+           println(fullName)
+    var age = 23
+    println(age)
+    var phoneNumber:String = "0759547300"
+    println(phoneNumber)
+    var weight = 56
+    println(weight)
+    var SheIsKenyan: Boolean=true
+    println(SheIsKenyan)
+
+
+
+}
